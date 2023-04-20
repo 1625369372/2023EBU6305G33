7a6bbe0fd356ba2291bd6405ad4b568c00a96e36
yongzhewang <w1625369372@gmail.com>
2023-04-20 15:29:33 +0800

    121

--- discover_beijing/js/main.js
+++ discover_beijing/js/main.js
@@ -21,4 +21,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function changeSlide() {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.opacity = 1;
+}
+
+setInterval(changeSlide, 5000); // 更改间隔时间，单位为毫秒，这里设置为 5000 毫秒（5 秒）
 
